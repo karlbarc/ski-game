d985a2b1c309a7cb05cbb1fbc6e1664712a28aa0
karlbarc <carlbarc@gmail.com>
2026-07-17 15:49:01 -0400

    deploy: v1784317741

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import * as THREE from 'three';
-import { buildTrack, mulberry32 } from './track.js?v=1784317028';
-import { verde } from './tracks/verde.js?v=1784317028';
-import { azul } from './tracks/azul.js?v=1784317028';
-import { negra } from './tracks/negra.js?v=1784317028';
-import { createPlayerState, stepPlayer, recoverPlayer, PARAMS } from './player.js?v=1784317028';
+import { buildTrack, mulberry32 } from './track.js?v=1784317741';
+import { verde } from './tracks/verde.js?v=1784317741';
+import { azul } from './tracks/azul.js?v=1784317741';
+import { negra } from './tracks/negra.js?v=1784317741';
+import { createPlayerState, stepPlayer, recoverPlayer, PARAMS } from './player.js?v=1784317741';
 import {
   createRace, updateRace, pauseRace, resumeRace, formatTime,
   loadBest, saveBest, loadBestSpeed, saveBestSpeed,
-} from './race.js?v=1784317028';
-import { createControls } from './controls.js?v=1784317028';
-import { createHud } from './hud.js?v=1784317028';
-import { createSnowSound } from './audio.js?v=1784317028';
+} from './race.js?v=1784317741';
+import { createControls } from './controls.js?v=1784317741';
+import { createHud } from './hud.js?v=1784317741';
+import { createSnowSound } from './audio.js?v=1784317741';
 
 const query = new URLSearchParams(location.search);
 const AUTOPILOT = query.get('autopilot') === '1';
